6796a56b74b8081fbfa835c63d181a4c47cff8fa
jabbatheteus <patorockeiro62@gmail.com>
2025-10-07 12:23:46 -0300

    Add files via upload

---
+++ mateus_1d/mural.php
@@ -0,0 +1,14 @@
+<?php
+ include "conexao.php";
+
+    if(isset($_POST['cadastra'])){
+    $nome  = mysqli_real_escape_string($conexao, $_POST['nome']);
+    $email = mysqli_real_escape_string($conexao, $_POST['email']);
+    $msg   = mysqli_real_escape_string($conexao, $_POST['msg']);
+
+    $sql = "INSERT INTO mateus_1d (nome, email, mensagem) VALUES ('$nome', '$email', '$msg')";
+    mysqli_query($conexao, $sql) or die("Erro ao inserir dados: " . mysqli_error($conexao));
+    header("Location: mural.php");
+    exit;
+}
+?>
